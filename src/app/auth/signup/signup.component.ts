b7a4701b7ca042938c02e173e374b6ba7089318b
UrosVesic <uros99uki@gmail.com>
2022-06-19 17:44:11 +0200

    Obrisan import za FormBuilder

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { SignupRequestPaylaod } from './signup-request.payload';
 
