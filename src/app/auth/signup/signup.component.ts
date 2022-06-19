119d89673df578da02803d03dc3eec42b7c35b59
UrosVesic <uros99uki@gmail.com>
2022-06-19 02:45:09 +0200

    Dodati atributi i implementirana metoda signup()

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { SignupRequestPaylaod } from './signup-request.payload';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  signupRequestPayload: SignupRequestPaylaod
+
+
+
+  constructor(private authService:AuthService) { 
+    this.signupForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required)
+    })
+    this.signupRequestPayload={
+      username:'',
+      email:'',
+      password:''
+    }
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  signup(){
+    this.signupRequestPayload.email = this.signupForm.get('email')!.value;
+    this.signupRequestPayload.username = this.signupForm.get('username')!.value;
+    this.signupRequestPayload.password = this.signupForm.get('password')!.value;
+    this.authService.signup(this.signupRequestPayload).subscribe(data=>{
+      console.log(data);
+    })
   }
 
 }
