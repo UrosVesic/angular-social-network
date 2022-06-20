e0014260f5711fae327c5fc86528f8cdc42aa07f
UrosVesic <uros99uki@gmail.com>
2022-06-21 00:01:18 +0200

    Formatiran

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import{HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { SignupRequestPaylaod } from '../signup/signup-request.payload';
 import { map, Observable } from 'rxjs';
 import { LocalStorageService } from 'ngx-webstorage';
@@ -7,25 +7,37 @@ import { LoginRequestPayload } from '../login/login-request.payload';
 import { LoginResponse } from '../login/login-response.payload';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
+  constructor(
+    private httpClient: HttpClient,
+    private localStorage: LocalStorageService
+  ) {}
 
-  constructor(private httpClient:HttpClient,private localStorage: LocalStorageService) { }
-
-  signup(signupRequestPayload: SignupRequestPaylaod) :Observable<any>{
-    return this.httpClient.post('http://localhost:8080/api/auth/signup',signupRequestPayload,{responseType:'text'});
+  signup(signupRequestPayload: SignupRequestPaylaod): Observable<any> {
+    return this.httpClient.post(
+      'http://localhost:8080/api/auth/signup',
+      signupRequestPayload,
+      { responseType: 'text' }
+    );
   }
-
 
   login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
-    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
-      .pipe(map(data => {
-        this.localStorage.store('authenticationToken', data.authenticationToken);
-        this.localStorage.store('username', data.username);
-        return true;
-      }));
+    return this.httpClient
+      .post<LoginResponse>(
+        'http://localhost:8080/api/auth/login',
+        loginRequestPayload
+      )
+      .pipe(
+        map((data) => {
+          this.localStorage.store(
+            'authenticationToken',
+            data.authenticationToken
+          );
+          this.localStorage.store('username', data.username);
+          return true;
+        })
+      );
   }
-
 }
