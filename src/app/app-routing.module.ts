91059578fa94eefe5e643f31b3546cfb1f8dea14
UrosVesic <uros99uki@gmail.com>
2022-06-20 15:35:09 +0200

    Dodata login ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'sign-up', component: SignupComponent }
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
