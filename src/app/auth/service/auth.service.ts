ccfbac242f8a052bb04854802c177bbe991bf5fe
UrosVesic <uros99uki@gmail.com>
2022-06-20 15:37:04 +0200

    Dodata metoda login

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http';
 import { SignupRequestPaylaod } from '../signup/signup-request.payload';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { LocalStorageService } from 'ngx-webstorage';
+import { LoginRequestPayload } from '../login/login-request.payload';
+import { LoginResponse } from '../login/login-response.payload';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient,private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPaylaod) :Observable<any>{
     return this.httpClient.post('http://localhost:8080/api/auth/signup',signupRequestPayload,{responseType:'text'});
   }
+
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
+      .pipe(map(data => {
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        return true;
+      }));
+  }
+
 }
