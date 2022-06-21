69eedf4c89d709d4cb74e863705b4b9a31e60209
UrosVesic <uros99uki@gmail.com>
2022-06-21 12:50:55 +0200

    Dodat post-model koji predstavlja post response od servera

---
+++ src/app/service/post-model.ts
@@ -0,0 +1,12 @@
+export interface PostModel {
+  id: number;
+  postName: string;
+  url: string;
+  content: string;
+  likes: number;
+  dislikes: number;
+  userName: string;
+  topicName: string;
+  commentCount: number;
+  duration: string;
+}
