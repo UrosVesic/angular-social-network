23d148c682a60106638790936885aa20d78730f0
UrosVesic <uros99uki@gmail.com>
2022-06-22 03:05:33 +0200

    Dodata metoda createTopic(TopicModel)

--- src/app/topic/topic.service.ts
+++ src/app/topic/topic.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TopicModel } from './topic-response';
+import { TopicModel } from './topic-model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +12,13 @@ export class TopicService {
   getAllTopics(): Observable<Array<TopicModel>> {
     return this.http.get<Array<TopicModel>>(
       'http://localhost:8080/api/topic/all'
+    );
+  }
+
+  createTopic(topicModel: TopicModel): Observable<TopicModel> {
+    return this.http.post<TopicModel>(
+      'http://127.0.0.1:8080/api/topic/create',
+      topicModel
     );
   }
 }
