40aec98db0ed9882e55efe6153968e391b1ddd25
UrosVesic <uros99uki@gmail.com>
2022-06-22 02:59:52 +0200

    Dodata komponenta za kreiranje postova

---
+++ src/app/post/create-post/create-post.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreatePostComponent } from './create-post.component';
+
+describe('CreatePostComponent', () => {
+  let component: CreatePostComponent;
+  let fixture: ComponentFixture<CreatePostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreatePostComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreatePostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/post/create-post/create-post.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { PostService } from 'src/app/service/post.service';
+import { TopicModel } from 'src/app/topic/topic-model';
+import { TopicService } from 'src/app/topic/topic.service';
+import { PostRequest } from './post-request';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css'],
+})
+export class CreatePostComponent implements OnInit {
+  createPostForm: FormGroup;
+  topics: Array<TopicModel> = [];
+  postRequest: PostRequest;
+
+  constructor(
+    private router: Router,
+    private topicService: TopicService,
+    private postService: PostService
+  ) {
+    this.createPostForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      url: new FormControl(''),
+      topicName: new FormControl(''),
+      content: new FormControl(''),
+    });
+    this.postRequest = {
+      title: '',
+      content: '',
+      topicName: '',
+    };
+  }
+
+  ngOnInit(): void {
+    this.topicService.getAllTopics().subscribe({
+      next: (data) => {
+        this.topics = data;
+      },
+      error: (error) => throwError(() => error),
+    });
+  }
+  discardPost() {
+    this.router.navigateByUrl('/');
+  }
+  createPost() {
+    this.postRequest.title = this.createPostForm.get('title')!.value;
+    this.postRequest.content = this.createPostForm.get('content')!.value;
+    this.postRequest.topicName = this.createPostForm.get('topicName')!.value;
+
+    this.postService.createPost(this.postRequest).subscribe({
+      next: (data) => {
+        this.router.navigateByUrl('/');
+        console.log(data);
+      },
+      error: (error) => {
+        throwError(() => error);
+        console.log(error);
+      },
+    });
+  }
+}
