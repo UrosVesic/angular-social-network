0a3e3798bf149a95c026441161da12ee0dfa1046
UrosVesic <uros99uki@gmail.com>
2022-06-23 00:14:58 +0200

    PostModel dto

---
+++ src/app/post/post-model.ts
@@ -0,0 +1,12 @@
+export interface PostModel {
+  id: number;
+  title: string;
+  content: string;
+  likes: number;
+  dislikes: number;
+  userName: string;
+  topicName: string;
+  commentCount: number;
+  duration: string;
+  likeCount: number;
+}
