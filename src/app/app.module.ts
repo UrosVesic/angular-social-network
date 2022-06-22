f7e7ab2a914003a0b290ac6703139a0c20adefea
UrosVesic <uros99uki@gmail.com>
2022-06-22 02:57:37 +0200

    Dodate komponente u NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './auth/login/login.component';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +15,12 @@ import { PostComponent } from './shared/post/post.component';
 import { ReactionComponent } from './shared/reaction/reaction.component';
 import { SideComponent } from './shared/side/side.component';
 import { TopicSideComponent } from './shared/topic-side/topic-side.component';
+import { CreateTopicComponent } from './topic/create-topic/create-topic.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ListTopicsComponent } from './topic/list-topics/list-topics.component';
+import { TokenIntercepton } from './auth/token-interceptor';
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { ViewPostComponent } from './post/view-post/view-post.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +33,10 @@ import { TopicSideComponent } from './shared/topic-side/topic-side.component';
     ReactionComponent,
     SideComponent,
     TopicSideComponent,
+    CreateTopicComponent,
+    CreatePostComponent,
+    ListTopicsComponent,
+    ViewPostComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,8 +45,15 @@ import { TopicSideComponent } from './shared/topic-side/topic-side.component';
     ReactiveFormsModule,
     HttpClientModule,
     NgxWebstorageModule.forRoot(),
+    EditorModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenIntercepton,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
