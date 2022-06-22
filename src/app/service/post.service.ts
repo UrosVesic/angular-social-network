da39f934e7b7a741b8548091ecd74aada856048c
UrosVesic <uros99uki@gmail.com>
2022-06-22 03:01:32 +0200

    Dodate metode createPost i getPost(id)

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PostRequest } from '../post/create-post/post-request';
 import { PostModel } from './post-model';
 
 @Injectable({
@@ -12,6 +13,19 @@ export class PostService {
   getAllPosts(): Observable<Array<PostModel>> {
     return this.httpClient.get<Array<PostModel>>(
       'http://localhost:8080/api/post/all'
+    );
+  }
+
+  createPost(postRequest: PostRequest): Observable<PostModel> {
+    return this.httpClient.post<PostModel>(
+      'http://localhost:8080/api/post/create',
+      postRequest
+    );
+  }
+
+  getPost(id: number): Observable<PostModel> {
+    return this.httpClient.get<PostModel>(
+      'http://localhost:8080/api/post/' + id
     );
   }
 }
