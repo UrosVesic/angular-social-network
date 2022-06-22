8ba27bd003f23eb5153fcaf05226872f514045bb
UrosVesic <uros99uki@gmail.com>
2022-06-22 02:57:18 +0200

    Dodate nove rute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
+import { CreateTopicComponent } from './topic/create-topic/create-topic.component';
+import { ListTopicsComponent } from './topic/list-topics/list-topics.component';
 
 const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-topic', component: CreateTopicComponent },
+  { path: 'list-topics', component: ListTopicsComponent },
+  { path: 'view-post/:id', component: ViewPostComponent },
 ];
 
 @NgModule({
