5648507dd3885158a9e8c7d90a40647eecc656cf
UrosVesic <uros99uki@gmail.com>
2022-06-23 00:14:43 +0200

    Dodata commentModel klasa i commentService

---
+++ src/app/comment/comment-model.ts
@@ -0,0 +1,6 @@
+export interface CommentModel {
+  postId: number;
+  text: string;
+  username: string;
+  duration: string;
+}
---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommentModel } from './comment-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  constructor(private httpClient: HttpClient) {}
+
+  postComment(commentModel: CommentModel): Observable<any> {
+    return this.httpClient.post(
+      'http://localhost:8080/api/comment/',
+      commentModel
+    );
+  }
+  getAllCommentsForPost(postId: number): Observable<CommentModel[]> {
+    return this.httpClient.get<CommentModel[]>(
+      'http://localhost:8080/api/comment/' + postId
+    );
+  }
+}
