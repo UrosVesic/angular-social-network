aea903050820b072373edaa859a199f9cf75e452
UrosVesic <uros99uki@gmail.com>
2022-06-24 23:21:55 +0200

    Dodata ruta i komponente  za prikaz korisnickog profila

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 import { HomeComponent } from './home/home.component';
 import { CreatePostComponent } from './post/create-post/create-post.component';
 import { ViewPostComponent } from './post/view-post/view-post.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'create-topic', component: CreateTopicComponent },
   { path: 'list-topics', component: ListTopicsComponent },
   { path: 'view-post/:id', component: ViewPostComponent },
+  { path: 'user-profile/:username', component: UserProfileComponent },
 ];
 
 @NgModule({
---
+++ src/app/auth/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { throwError } from 'rxjs';
+import { PostModel } from 'src/app/post/post-model';
+import { PostService } from 'src/app/post/service/post.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  username: string;
+  postLength: number;
+  posts: PostModel[] = [];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private postService: PostService
+  ) {
+    this.username = activatedRoute.snapshot.params['username'];
+    this.postLength = 0;
+  }
+
+  ngOnInit(): void {
+    this.postService.getAllPostsForUser(this.username).subscribe({
+      next: (data) => ((this.posts = data), (this.postLength = data.length)),
+      error: (error) => throwError(() => error),
+    });
+  }
+}
