fcb590450425b53ce2c36363a320693093ae4f1e
UrosVesic <uros99uki@gmail.com>
2022-06-24 23:25:04 +0200

    Implementirana logika za prikaz razlicitih elemenata
     u headeru u zavinsnosti da li je korisnik prijavljen ili ne

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth/service/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  faUser = faUser;
+  isLoggedIn: boolean;
+  username: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) {
+    this.username = '';
+    this.isLoggedIn = this.authService.isLogged();
   }
 
+  ngOnInit(): void {
+    this.authService.loggedIn.subscribe(
+      (data: boolean) => (this.isLoggedIn = data)
+    );
+    this.authService.username.subscribe(
+      (data: string) => (this.username = data)
+    );
+    this.isLoggedIn = this.authService.isLogged();
+    this.username = this.authService.getUserName();
+  }
+
+  goToUserProfile() {
+    this.router.navigateByUrl('/user-profile/' + this.username);
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    this.isLoggedIn = false;
+  }
 }
