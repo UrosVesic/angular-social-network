496e4a962ca3dacbba1cfc7cd7e2817189ed975d
UrosVesic <uros99uki@gmail.com>
2022-06-25 13:53:19 +0200

    Dodat AuthGard na neke rute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { UserProfileComponent } from './auth/user-profile/user-profile.component';
@@ -13,11 +14,23 @@ const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
-  { path: 'create-post', component: CreatePostComponent },
-  { path: 'create-topic', component: CreateTopicComponent },
+  {
+    path: 'create-post',
+    component: CreatePostComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'create-topic',
+    component: CreateTopicComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'list-topics', component: ListTopicsComponent },
   { path: 'view-post/:id', component: ViewPostComponent },
-  { path: 'user-profile/:username', component: UserProfileComponent },
+  {
+    path: 'user-profile/:username',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isAuthenticated = this.authService.isLogged();
+    if (isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+    return true;
+  }
+}
