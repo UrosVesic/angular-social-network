2e04020c9c907a5d589b0957b3dd6205b662594e
UrosVesic <uros99uki@gmail.com>
2022-06-25 13:59:38 +0200

    Implementirane neophodne metode

--- src/app/reaction/reaction.component.ts
+++ src/app/reaction/reaction.component.ts
@@ -4,7 +4,12 @@ import {
   faThumbsDown,
   faThumbsUp,
 } from '@fortawesome/free-solid-svg-icons';
+import { throwError } from 'rxjs';
 import { PostModel } from 'src/app/post/post-model';
+import { PostService } from '../post/service/post.service';
+import { ReactionModel } from './reaction-model';
+import { ReactionType } from './reaction-type';
+import { ReactionService } from './service/reaction.service';
 
 @Component({
   selector: 'app-reaction',
@@ -12,12 +17,16 @@ import { PostModel } from 'src/app/post/post-model';
   styleUrls: ['./reaction.component.css'],
 })
 export class ReactionComponent implements OnInit {
+  reactionModel: ReactionModel;
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
   faComments = faComments;
   @Input() post: PostModel;
 
-  constructor() {
+  constructor(
+    private reactionService: ReactionService,
+    private postService: PostService
+  ) {
     this.post = {
       id: 0,
       title: '',
@@ -28,12 +37,40 @@ export class ReactionComponent implements OnInit {
       duration: '',
       topicName: '',
       userName: '',
-      likeCount: 0,
+      liked: false,
+      disliked: false,
+    };
+    this.reactionModel = {
+      postId: 0,
+      reactionType: ReactionType.LIKE,
     };
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.updateReactDetails();
+  }
 
-  like() {}
-  dislike() {}
+  like() {
+    this.reactionModel.reactionType = ReactionType.LIKE;
+    this.react();
+  }
+
+  dislike() {
+    this.reactionModel.reactionType = ReactionType.DISLIKE;
+    this.react();
+  }
+
+  private react() {
+    this.reactionModel.postId = this.post.id;
+    this.reactionService.react(this.reactionModel).subscribe({
+      next: () => this.updateReactDetails(),
+      error: (error) => throwError(() => error),
+    });
+  }
+
+  private updateReactDetails() {
+    this.postService.getPost(this.post.id).subscribe((post) => {
+      this.post = post;
+    });
+  }
 }
