c57af5771ec25aa4ef2c2cfaf3558b322b5cc6f0
UrosVesic <uros99uki@gmail.com>
2022-07-04 00:17:16 +0200

    Implementirana panel komponenta

--- src/app/admin/panel/panel.component.ts
+++ src/app/admin/panel/panel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/auth/service/auth.service';
+import { PostModel } from 'src/app/post/post-model';
+import { ReportedPostModel } from 'src/app/post/reported-post-model';
+import { PostService } from 'src/app/post/service/post.service';
+import { ReportService } from 'src/app/report/report.service';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.css']
+  styleUrls: ['./panel.component.css'],
 })
 export class PanelComponent implements OnInit {
+  active = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
