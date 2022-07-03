78ac19369387da2ba27695b05e71c313801b6de1
UrosVesic <uros99uki@gmail.com>
2022-07-04 00:08:26 +0200

    Guard za rute kojima samo admin ima pristup

---
+++ src/app/admin/admin.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isAdmin = this.authService.isAdmin();
+    if (isAdmin) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/');
+    }
+    return true;
+  }
+}
