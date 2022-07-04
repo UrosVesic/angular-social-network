f6ef8cedd3610dff3fc9d8ebc1d17f8ba53e6332
UrosVesic <uros99uki@gmail.com>
2022-07-04 18:12:42 +0200

    Dodata klasa sa modalima

---
+++ src/app/modals.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Modals {
+  tinyAlert(message: string) {
+    Swal.fire(message);
+  }
+  successNotification() {
+    Swal.fire(
+      'Succesful',
+      'Succesfully reported, your report will be reviewed',
+      'success'
+    );
+    Swal.fire({
+      title: 'Succesful',
+      text: 'Succesfully reported, your report will be reviewed',
+      icon: 'success',
+      confirmButtonColor: '#1C9E5D',
+      confirmButtonText: 'OK',
+    });
+  }
+
+  errorNotification(message: string) {
+    /*Swal.fire('Cant report', message, 'error');*/
+    Swal.fire({
+      title: 'Cant report',
+      text: message,
+      icon: 'error',
+      confirmButtonColor: 'red',
+      confirmButtonText: 'OK',
+    });
+  }
+  alertConfirmation() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This process is irreversible.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, go ahead.',
+      cancelButtonText: 'No, let me think',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Removed!', 'Product removed successfully.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Product still in our database.)', 'error');
+      }
+    });
+  }
+}
