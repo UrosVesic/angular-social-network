d5bfeebd38f43d4baa1efa55afd5b71f22457a11
UrosVesic <uros99uki@gmail.com>
2022-07-10 00:52:24 +0200

    Prikaz modala prilikom greske

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,50 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Modals } from 'src/app/modals';
 import { AuthService } from '../service/auth.service';
 import { SignupRequestPaylaod } from './signup-request.payload';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
   signupForm: FormGroup;
-  signupRequestPayload: SignupRequestPaylaod
+  signupRequestPayload: SignupRequestPaylaod;
 
-
-
-  constructor(private authService:AuthService, private router:Router,
-   ) { 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private modals: Modals
+  ) {
     this.signupForm = new FormGroup({
-      username: new FormControl('',Validators.required),
-      email: new FormControl('',Validators.required),
-      password: new FormControl('',Validators.required)
-    })
-    this.signupRequestPayload={
-      username:'',
-      email:'',
-      password:''
-    }
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: '',
+    };
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
-  signup(){
+  signup() {
     this.signupRequestPayload.email = this.signupForm.get('email')!.value;
     this.signupRequestPayload.username = this.signupForm.get('username')!.value;
     this.signupRequestPayload.password = this.signupForm.get('password')!.value;
-    this.authService.signup(this.signupRequestPayload)
-    .subscribe(() => {
-      this.router.navigate(['/login'], { queryParams: { registered: 'true' } });
-    }, () => {
-      alert('Registration Failed! Please try again');
-      //this.toastr.error('Registration Failed! Please try again');
+    this.authService.signup(this.signupRequestPayload).subscribe({
+      next: () =>
+        this.router.navigate(['/login'], {
+          queryParams: { registered: 'true' },
+        }),
+      error: (error) => this.modals.errorNotification(error.error),
     });
   }
-
 }
