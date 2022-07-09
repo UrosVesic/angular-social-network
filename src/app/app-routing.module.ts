5275e5f56a02198c6dc7cff44b2b153bbbdea198
UrosVesic <uros99uki@gmail.com>
2022-07-10 00:50:58 +0200

    Dodata loginGuard na rutu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminGuard } from './admin/admin.guard';
 import { PanelComponent } from './admin/panel/panel.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoginGuard } from './auth/login.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { UserProfileComponent } from './auth/user-profile/user-profile.component';
@@ -18,7 +19,7 @@ import { AllUsersComponent } from './user/all-users/all-users.component';
 const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent, canActivate: [LoginGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'admin', component: PanelComponent, canActivate: [AdminGuard] },
   {
