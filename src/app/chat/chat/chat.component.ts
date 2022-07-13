7dd9ed9ad6eda676fb7efdce58bf3f4a4605b1e4
UrosVesic <uros99uki@gmail.com>
2022-07-13 23:22:24 +0200

    Implementirana chat funkcionalnost

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth/service/auth.service';
 import { StompService } from 'src/app/stomp-service';
 import { ChatService } from '../service/chat.service';
 import { Message } from './message';
@@ -13,35 +14,61 @@ import { Message } from './message';
 export class ChatComponent implements OnInit {
   messages: Message[] = [];
   messageToSend: string = '';
-  username: string = ''
+  username: string = '';
 
-  constructor(private stomp: StompService,
+  constructor(
+    private stomp: StompService,
     private chatService: ChatService,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((routeParams) => {
       this.username = routeParams['username'];
     });
-    this.stomp.subscribe('/topic', (): any => {
-      this.getAllMessages();
-    })
-    this.getAllMessages()
+    console.log(
+      '/topic/' + this.authService.getUserName() + '/' + this.username
+    );
+    this.stomp.subscribe(
+      '/topic/' + this.authService.getUserName(),
+      (): any => {
+        this.getLastMessage();
+      }
+    );
+    this.getAllMessagesFromChat();
   }
-  getAllMessages() {
-    this.chatService.getAllMessages().subscribe({
-      next: (data) => this.messages = data,
-      error: (error) => console.log(error)
-    })
+  getAllMessagesFromChat() {
+    this.chatService
+      .getAllMessagesFromChat(this.authService.getUserName(), this.username)
+      .subscribe({
+        next: (data) => (this.messages = data),
+        error: (error) => console.log(error),
+      });
   }
 
   send() {
     let message = new Message();
+    console.log(this.messageToSend);
     message.content = this.messageToSend;
+    message.from = this.authService.getUserName();
+    message.to = this.username;
     this.chatService.sendMessage(message).subscribe({
-      next: () => console.log("success"),
-      error: (error) => console.log(error)
-    })
+      next: () => this.messages.push(message),
+      error: (error) => console.log(error),
+    });
+    this.messageToSend = '';
   }
 
+  myMessage(msg: Message) {
+    return msg.from == this.authService.getUserName();
+  }
+
+  getLastMessage() {
+    this.chatService
+      .getLastMessage(this.authService.getUserName(), this.username)
+      .subscribe((data) => {
+        this.messages.push(data);
+      });
+  }
 }
--- src/app/chat/service/chat.service.ts
+++ src/app/chat/service/chat.service.ts
@@ -5,18 +5,30 @@ import { environment } from 'src/environments/environment';
 import { Message } from '../chat/message';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
   baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllMessages(): Observable<Array<Message>> {
     return this.http.get<Array<Message>>(this.baseUrl + 'api/message/all');
   }
 
   sendMessage(message: Message): Observable<any> {
-    return this.http.post(this.baseUrl + 'api/message', message)
+    return this.http.post(this.baseUrl + 'api/message/' + message.to, message);
+  }
+
+  getLastMessage(from: string, to: string) {
+    return this.http.get<Message>(
+      this.baseUrl + 'api/message/last/' + from + '/' + to
+    );
+  }
+
+  getAllMessagesFromChat(from: string, to: string): Observable<Array<Message>> {
+    return this.http.get<Array<Message>>(
+      this.baseUrl + 'api/message/all/' + from + '/' + to
+    );
   }
 }
