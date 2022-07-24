dab695b46730ea8e76adf82d2df0074f5927f231
UrosVesic <uros99uki@gmail.com>
2022-07-25 00:08:41 +0200

    Dodate 3 nove metode

--- src/app/chat/service/chat.service.ts
+++ src/app/chat/service/chat.service.ts
@@ -12,8 +12,13 @@ import { InboxMessage } from '../inbox-message';
 export class ChatService {
   baseUrl = environment.baseUrl;
   @Output() sentMessage: EventEmitter<string> = new EventEmitter();
+  @Output() numberOfSeenMessages: EventEmitter<number> = new EventEmitter();
 
   constructor(private http: HttpClient, private authService: AuthService) {}
+
+  emitNumberOfSeenMessages(seenMessages: number) {
+    this.numberOfSeenMessages.emit(seenMessages);
+  }
 
   getAllMessages(): Observable<Array<MessageDto>> {
     return this.http.get<Array<MessageDto>>(this.baseUrl + 'api/message/all');
@@ -52,5 +57,13 @@ export class ChatService {
     return this.http.get<Array<InboxMessage>>(
       this.baseUrl + 'api/message/inbox'
     );
+  }
+
+  getNumberOfNewMsg(): Observable<number> {
+    return this.http.get<number>(this.baseUrl + 'api/message/new-msg-count');
+  }
+
+  readMessages(username: string) {
+    return this.http.patch(this.baseUrl + 'api/message/read/' + username, '');
   }
 }
